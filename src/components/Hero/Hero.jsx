c0259af4952b1da8215e9febcf4a779157e3a5ee
nawafnaofal <nawafnaufal1@gmail.com>
2021-07-10 23:13:31 +0700

    nama

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Halo, nama saya'}{' '}
-            <span className="text-color-main">{name || ''}</span>
+            <span className="text-color-main">{name || 'Nawaf Naofal'}</span>
             <br />
             {subtitle || "Saya adalah mahasiswa Politeknik Pos Indonesia."}
           </h1>
--- src/mock/data.js
+++ src/mock/data.js
@@ -10,7 +10,7 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: 'Nawaf Naofall',
+  name: '',
   subtitle: '',
   cta: '',
 };
